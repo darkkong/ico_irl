4f58637d83aa215990c23e655fa5ed7c2b623a2a
Hyojung Kim <darkkong88@gmail.com>
2021-05-21 23:05:57 +0900

    #2 Build a Mintable Token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract DappToken is ERC20, ERC20Detailed {
+contract DappToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
   constructor(
     string memory _name,
     string memory _symbol,
